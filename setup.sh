c30e327e13b4c3fac17639bfb82904c9985142cd
saanpritom <pritom.saan@gmail.com>
2022-11-23 00:50:38 +0600

    env variables and static files are configured.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
-#!/usr/bin/bash
-
+#!/bin/sh
+set -e
 python app/manage.py makemigrations
 python app/manage.py migrate
+python app/manage.py collectstatic --no-input
 python app/manage.py createsuperuser --no-input
+exec "$@"
