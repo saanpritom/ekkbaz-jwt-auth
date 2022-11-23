2df1847a439d12cc128c64dc29e84b5d99086b01
saanpritom <pritom.saan@gmail.com>
2022-11-23 17:11:00 +0600

    setup file is updated

--- setup.sh
+++ setup.sh
@@ -4,5 +4,5 @@ python app/manage.py makemigrations
 python app/manage.py migrate
 python app/manage.py collectstatic --no-input
 python app/manage.py createdefaultuser
-gunicorn --workers 5 --timeout 60 --bind 0.0.0.0:8000 app.conf.wsgi:application --daemon
+sh -c 'cd app/ && gunicorn --workers 5 --timeout 60 --bind 0.0.0.0:8000 app.conf.wsgi:application --daemon'
 exec "$@"
